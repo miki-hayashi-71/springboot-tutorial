4dff50935fc65d5091bb0eba53ab6f853f591e84
miki-hayashi-71 <miki.hayashi.71+work@gmail.com>
2025-07-10 10:56:37 +0900

    既存のコードをLombokに書き換え

--- src/main/java/com/example/springboot_tutorial/model/Item.java
+++ src/main/java/com/example/springboot_tutorial/model/Item.java
@@ -4,37 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data // ＠@Getter, @Setter, @ToStringなどをまとめて設定
+@AllArgsConstructor // 全フィールドを引数に持つコンストラクター
+@NoArgsConstructor // 引数なしのコンストラクター(JPAに必要)
 @Entity // このクラスがDBのテーブルに対応することを示す
 public class Item {
 
-    @Id // テーブルn主キーであることを明示
+    @Id // テーブルに主キーであることを明示
     @GeneratedValue(strategy = GenerationType.IDENTITY) // ＠Idとセットで使用、IDを自動採番
     private Long id;
     private String name;
     private Integer price;
-
-    // 引数なしコンストラクタ(JPAのため)
-    public Item() {
-    }
-
-    // コンストラクタ
-    public Item(Long id, String name, Integer price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-
-    // privateなフィールドの値を取得するた目のゲッターメソッド
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
 }
