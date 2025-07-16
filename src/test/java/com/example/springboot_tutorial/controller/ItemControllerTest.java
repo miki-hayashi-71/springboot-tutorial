ca631549b89a3d75a0487fb1ecb05e7935a29468
miki-hayashi-71 <miki.hayashi.71+work@gmail.com>
2025-07-16 10:53:03 +0900

    誤って削除していたコードを追加

--- src/test/java/com/example/springboot_tutorial/controller/ItemControllerTest.java
+++ src/test/java/com/example/springboot_tutorial/controller/ItemControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +23,8 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest(ItemController.class) // ItemControllerのみをテスト対象にする
+class ItemControllerTest {
     // HTTPリクエストを送信するためのクライアント
     @Autowired
     private MockMvc mockMvc;
